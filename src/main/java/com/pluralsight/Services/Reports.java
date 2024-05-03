bf53b310d3007531d40a692aa66f56d293e215d2
Sydney Morano <syd.morano@gmail.com>
2024-05-03 14:06:18 -0700

    Last MIN UPDATES: peers inspired me during presentations -- ADDING ALL TRANSACTIONS INVOLVED WITH ALL REPORTS NOT JUST SEARCHES

--- src/main/java/com/pluralsight/Services/Reports.java
+++ src/main/java/com/pluralsight/Services/Reports.java
@@ -4,6 +4,7 @@ import com.pluralsight.Models.Transaction;
 import com.pluralsight.Models.TransactionList;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 
 public class Reports {
     private final TransactionList transactions;
@@ -11,24 +12,76 @@ public class Reports {
     public Reports(TransactionList transactions) {
         this.transactions = transactions;
     }
+
+    // LAST MINUTE UPDATES: I wanted to show all the transactions involved with MTD/YTD/lastMonth/lastYear
+    // I was inspired by peers.
+    // original code is commented out.
+
     public double monthToDate(LocalDateTime today) {
-        return transactions.stream()
+        ArrayList<Transaction> filtered = new ArrayList<>();
+        transactions.stream()
+                .filter(transaction -> transaction.getDateTime().getYear() == today.getYear())
+                .filter(transaction -> transaction.getDateTime().getMonthValue() == today.getMonthValue())
+                .filter(transaction -> transaction.getDateTime().getDayOfMonth() <= today.getDayOfMonth())
+                .forEach(transaction -> {
+                    filtered.add(transaction);
+                    transaction.display();
+                });
+        System.out.println();
+        return filtered.stream()
+                .mapToDouble(Transaction::getAmount)
+                .sum();
+        /*return transactions.stream()
                 .filter(transaction -> transaction.getDateTime().getYear() == today.getYear())
                 .filter(transaction -> transaction.getDateTime().getMonthValue() == today.getMonthValue())
                 .filter(transaction -> transaction.getDateTime().getDayOfMonth() <= today.getDayOfMonth())
                 .mapToDouble(Transaction::getAmount)
-                .sum();
+                .sum();*/
     }
     public double lastMonth(LocalDateTime today) {
-        return transactions.stream()
+        ArrayList<Transaction> filtered = new ArrayList<>();
+        transactions.stream()
+                .filter(transaction -> transaction.getDateTime().getYear() == today.getYear())
+                .filter(transaction -> transaction.getDateTime().getMonthValue() == today.getMonthValue()-1)
+                .forEach(transaction -> {
+                    filtered.add(transaction);
+                    transaction.display();
+                });
+        System.out.println();
+        return filtered.stream()
+                .mapToDouble(Transaction::getAmount)
+                .sum();
+        /*return transactions.stream()
                 .filter(transaction -> transaction.getDateTime().getYear() == today.getYear())
                 .filter(transaction -> transaction.getDateTime().getMonthValue() == today.getMonthValue()-1)
                 .mapToDouble(Transaction::getAmount)
-                .sum();
+                .sum();*/
     }
     public double yearToDate(LocalDateTime today) {
         // MTD + Beginning of Year to Before Current Month
-        return (transactions.stream()
+        ArrayList<Transaction> filtered = new ArrayList<>();
+        // Beginning of Year Before current Month
+        transactions.stream()
+                .filter(transaction -> transaction.getDateTime().getYear() == today.getYear())
+                .filter(transaction -> transaction.getDateTime().getMonthValue() < today.getMonthValue())
+                .forEach(transaction -> {
+                    filtered.add(transaction);
+                    transaction.display();
+                });
+        // MTD
+        transactions.stream()
+                .filter(transaction -> transaction.getDateTime().getYear() == today.getYear())
+                .filter(transaction -> transaction.getDateTime().getMonthValue() == today.getMonthValue())
+                .filter(transaction -> transaction.getDateTime().getDayOfMonth() <= today.getDayOfMonth())
+                .forEach(transaction -> {
+                    filtered.add(transaction);
+                    transaction.display();
+                });
+        System.out.println();
+        return filtered.stream()
+                .mapToDouble(Transaction::getAmount)
+                .sum();
+        /*return (transactions.stream()
                 .filter(transaction -> transaction.getDateTime().getYear() == today.getYear())
                 .filter(transaction -> transaction.getDateTime().getMonthValue() == today.getMonthValue())
                 .filter(transaction -> transaction.getDateTime().getDayOfMonth() <= today.getDayOfMonth())
@@ -37,13 +90,24 @@ public class Reports {
                 .filter(transaction -> transaction.getDateTime().getYear() == today.getYear())
                 .filter(transaction -> transaction.getDateTime().getMonthValue() < today.getMonthValue())
                 .mapToDouble(Transaction::getAmount)
-                .sum()) ;
+                .sum()) ;*/
     }
     public double lastYear(LocalDateTime today) {
-        return transactions.stream()
+        ArrayList<Transaction> filtered = new ArrayList<>();
+        // Beginning of Year Before current Month
+        transactions.stream()
                 .filter(transaction -> transaction.getDateTime().getYear() == today.getYear()-1)
+                .forEach(transaction -> {
+                    filtered.add(transaction);
+                    transaction.display();
+                });
+        return filtered.stream()
                 .mapToDouble(Transaction::getAmount)
                 .sum();
+        /*return transactions.stream()
+                .filter(transaction -> transaction.getDateTime().getYear() == today.getYear()-1)
+                .mapToDouble(Transaction::getAmount)
+                .sum();*/
     }
     // Search Reports
     public void searchVendor(String input) {
